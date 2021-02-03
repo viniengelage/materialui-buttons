9cfb742280450d74b7ae8ce7a949747895c44641
Vinicios Engelage <viniciosvalensuela@gmail.com>
2021-02-03 11:35:31 -0300

    Config json

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 import ButtonComponent, { ButtonProps } from 'components/Button'
 
-export { ButtonProps } from 'components/Button'
-
 import defaultTheme from 'styles/theme'
 
 let theme = defaultTheme;
 
-export function addButtonTheme(userTheme: any) {
+function addButtonTheme(userTheme: any) {
   return theme = userTheme;
 }
 
@@ -17,5 +15,10 @@ const Button:React.FC<ButtonProps> = ({children, ...rest}) => {
     <ButtonComponent theme={theme} {...rest}>{children}</ButtonComponent>
   )
 };
+
+export {
+  addButtonTheme,
+  ButtonProps
+}
 
 export default Button;
